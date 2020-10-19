3845a920dbf3d83f3991cafa5bedc02235f06e7a
salindholm <sannelindholm@hotmail.com>
2020-10-19 12:30:25 +0200

    Adding a div for the contact list and connecting app.js to my index.html file. Adding new test for when create a contact it will show in app, spec failed.

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -7,7 +7,16 @@ describe('user can create a contact', () => {
 		cy.get('#phone').type('0700 101010')
 		cy.get('#company').type('Craft Academy')
 		cy.get('#notes').type('Awesome coder')
-		cy.get('#twitter').type('@thomasochman')
+        cy.get('#twitter').type('@thomasochman')
+        cy.get('#submit').click()
     })
+
+    it('displays a name of the new contact', () => {
+		cy.get('#contact-list').should('contain', 'Thomas')
+	})
+	
+	it('displays the phone number of the new contact', () => {
+		cy.get('#contact-list').should('contain', '0700 101010')
+	})
     
 })
